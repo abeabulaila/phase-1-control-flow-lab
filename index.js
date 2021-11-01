078caeb981ab6ae9afd3c2658451a122f81521e5
Ibrahim Abulaila <abeabulaila@gmail.com>
2021-11-01 12:37:01 -0400

    commit tree 0f14466324c97ed6b97fb68c29bc388179f3bdd8

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+
+function scuberGreetingForFeet(feetNumber){
+  let result
+  if (feetNumber <= 400) {
+    result = feetNumber; 
+  return ("This one is on me!"); 
+  }
+  else if (feetNumber > 2500) {
+    result = feetNumber;
+    return ("No can do.");
+  }
+  else if (feetNumber > 2000) {
+    result = feetNumber;
+    return ("I will gladly take your thirty bucks.");
+  }
+
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+
+function ternaryCheckCity(cityName){
+  return cityName === 'NYC'?'Ok, sounds good.' : "No go.";
+
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(money){
+    switch(money){
+      case "generous":
+          return "Thank you so much.";
+          case "not as generous":
+              return "Thank you.";
+              default:
+                return "Bye.";
+    }
 }
